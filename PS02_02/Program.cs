dec75081d9678efee842aa81c2c934d732c5a3bf
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-08-21 10:57:27 +0800

    added Practice02

---
+++ PS02_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            * Write a program that prints all the odd numbers from 1 to 20 using a for loop. 
+            */
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Practice Sets 02
+
+## Activities
+- Write a program that prints all the even numbers from 1 to 20 using a for loop.
+- Write a program that prints all the odd numbers from 1 to 20 using a for loop.
+
+## Authors
+
+
