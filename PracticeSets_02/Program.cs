0b4b352622d57996856a42e253ec4a906ffc9624
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-08-21 11:06:46 +0800

    added Even Code

--- PracticeSets_02/Program.cs
+++ PracticeSets_02/Program.cs
@@ -4,20 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PS02_02
+namespace PracticeSets_02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 20; i++){
+            // Print even numbers from 1 - 20
+                for (int i = 0; i <= 20; i++){
                 if(i % 2 == 0){
-                    System.Console.Write(i + " ");
+                    System.Console.Write(i);
                 }
             }
-            /*
-            * Write a program that prints all the odd numbers from 1 to 20 using a for loop. 
-            */
         }
     }
 }
