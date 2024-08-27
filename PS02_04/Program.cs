36466821f3efadf69cd440b1a0a23ce2857bebce
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-08-27 10:06:18 +0800

    Added template PS02_03 - PS02_07

---
+++ PS02_04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS02_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
