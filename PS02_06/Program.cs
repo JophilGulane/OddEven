7afceda3f968b53a6e0821db2a597606c2dbb42c
DevJophil <111653199+JophilGulane@users.noreply.github.com>
2024-09-24 05:50:47 +0800

    Update Program.cs

--- PS02_06/Program.cs
+++ PS02_06/Program.cs
@@ -11,33 +11,18 @@ namespace PS02_07
         static void Main(string[] args)
         {
             //Write a program that takes an array of numbers as input and outputs the sum of the numbers. 
-            Console.Write("Enter Array Length: ");
-            int arrayLength = int.Parse(Console.ReadLine());
-            int[] numbers = new int[arrayLength];
-
-            for (int i = 0; i < arrayLength; i++)
-            {
-                Console.Write($"Enter element at index {i}: ");
-                numbers[i] = int.Parse(Console.ReadLine());
-            }
-            Console.Write("Your array is: ");
-
-            foreach (int ele in numbers)
-            {
-                Console.Write(ele + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine($"Sum of Numbers in Array is = {sumOfArrayNum(numbers)}");
-        }
-
-        static int sumOfArrayNum(int[] numbers)
-        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+        
             int sum = 0;
-            foreach (int number in numbers)
+        
+            while (number != 0)
             {
-                sum += number;
+                sum += number % 10;
+                number /= 10;
             }
-            return sum;
+        
+            Console.WriteLine("Sum of the digits: " + sum);
         }
     }
 }
